b9dfac1303979f761c0b85b5fa83409782877d21
Owolabi shinaayomi <100287793+koredeowoblow@users.noreply.github.com>
2026-06-18 14:38:11 +0100

    Update dbTest2.js

--- scripts/dbTest2.js
+++ scripts/dbTest2.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 async function test() {
-  await mongoose.connect('mongodb+srv://koredeowolabi62_db_user:xHq7QjCCvYmsayiE@cluster0.swuupa3.mongodb.net/teampillar?appName=Cluster0');
+  await mongoose.connect('');
   const TopicPerformance = mongoose.connection.db.collection('topicperformances');
   const docs = await TopicPerformance.find({ totalAttempted: { $gt: 0 } }).limit(10).toArray();
   console.log(JSON.stringify(docs, null, 2));
